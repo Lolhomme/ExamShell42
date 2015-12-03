1c36c7aafa55466f96d58fb90b51bdc2d5defdc5
Anthony LAULOM <alaulom@e3r1p5.42.fr>
2015-12-03 12:28:27 +0100

    opti

--- rotone/rotone.c
+++ rotone/rotone.c
@@ -5,57 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/30 15:32:40 by alaulom           #+#    #+#             */
-/*   Updated: 2015/11/30 17:09:57 by alaulom          ###   ########.fr       */
+/*   Created: 2015/12/03 11:31:32 by alaulom           #+#    #+#             */
+/*   Updated: 2015/12/03 12:23:16 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		isupper(int c)
+void		ft_putchar(int c)
 {
-	return (c >= 'A' && c < 'Z');
+	write (1, &c, 1);
 }
 
-int		islower(int c)
+int			main(int argc, char **argv)
 {
-	return (c >= 'a' && c < 'z');
-}
-
-int		main(int argc, char **argv)
-{
-	int	i;
+	int		i;
 
 	i = 0;
-	if (argc == 2)
+	if(argc == 2)
 	{
 		while (argv[1][i])
 		{
-			if (islower(argv[1][i]))
-			{
-				if (argv[1][i] == 'z')
-					write (1, "a", 1);
-				else
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 				{
-					argv[1][i]++;
-					write (1, &argv[1][i], 1);
+					if	(argv[1][i] == 'z')
+						ft_putchar('a');
+					else
+						ft_putchar(argv[1][i] + 1);
 				}
-			}
-			else if (isupper(argv[1][i]))
-			{
-				if (argv[1][i] == 'Z')
-					write (1, "A", 1);
-				else
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 				{
-					argv[1][i]++;
-					write (1, &argv[1][i], 1);
+					if (argv[1][i] == 'Z')
+						ft_putchar('A');
+					else
+						ft_putchar(argv[1][i] + 1);
 				}
-			}
 			else
-				write(1, &argv[1][i], 1);
+				ft_putchar(argv[1][i]);
 			i++;
 		}
 	}
-	write (1, "\n", 1);	
-	return (0);
+	ft_putchar('\n');
+	return(0);
 }
